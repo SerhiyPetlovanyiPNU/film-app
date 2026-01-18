91cf5746b267191bd1d0c15e9162daf8e9e4ec2f
SerhiyPetlovanyiPNU <volodymyr-serhii.petlovanyi.23@pnu.edu.ua>
2026-01-18 23:14:10 +0200

    add routing & modules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Movie } from './pages/movie/movie';
+import { Series } from './pages/series/series';
+import { Episode } from './pages/episode/episode';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home, title: 'Home' },
+  { path: 'movie', component: Movie, title: 'Movie' },
+  { path: 'series', component: Series, title: 'Series' },
+  { path: 'episode', component: Episode, title: 'Episode' },
+
+  { path: '**', redirectTo: '' },
+];
---
+++ src/app/pages/episode/episode.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-episode',
+  imports: [],
+  templateUrl: './episode.html',
+  styleUrl: './episode.scss',
+})
+export class Episode {
+
+}
